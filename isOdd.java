b63a0f6e39b21e385c4d28ae4a6fdd5771e7f994
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-06-27 19:58:15 +0530

    Create isOdd.java

---
+++ isOdd.java
@@ -0,0 +1,22 @@
+package prp;
+import java.util.Scanner;
+import java.lang.*;
+public class Prp {
+    public static int isOdd(int a)
+    {
+        if(Math.abs(a)%2==1 && Math.abs(a)!=0 )
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int res=isOdd(a);
+        System.out.println(res);
+    }
+}
