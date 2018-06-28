64e0120fe4708839845526caeaa1997b616ba3b9
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-06-28 19:47:08 +0530

    Update 1.java

--- task8/1.java
+++ task8/1.java
@@ -1,22 +1,42 @@
-package add_string;
+package pkg1stprg;
 import java.util.Scanner;
-public class Add_string {
+public class Main {
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
        String one=sc.nextLine();
        String two=sc.nextLine();
        StringBuilder onee=new StringBuilder(one);
-       StringBuilder twoo=new StringBuilder(one);
+       StringBuilder twoo=new StringBuilder(two);
        one=onee.reverse().toString();
        two=twoo.reverse().toString();
        
-       for(int i=0;i<one.length();i++)
+       int maxlen=one.length()>two.length()?one.length():two.length();
+
+       String op="";int carry=0;
+       for(int i=0;i<maxlen;i++)
        {
-           int a=Character.getNumericValue(one.charAt(i));
-           int b=Character.getNumericValue(two.charAt(i));
-           int res=a+b;
-           
-       }
+           int a=0,b=0;
+           if(i<one.length())
+           {
+               a=Character.getNumericValue(one.charAt(i));
+           }
+           if(i<two.length())
+           {
+              b=Character.getNumericValue(two.charAt(i)); 
+           }
+           int res=a+b+carry;
+           if(res>=10)
+           {
+               op+=String.valueOf(res%10);
+               carry=res/10;
+           }
+           else
+           {
+               op+=String.valueOf(res);
+               carry=0;
+           }
+        }
+       StringBuilder out=new StringBuilder(op);
+       System.out.println(out.reverse());
     }
-    
 }
