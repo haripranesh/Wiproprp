4b82a49446dfd52b4ccaf46f191ffe4a6a9f0ac2
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-06-29 20:28:24 +0530

    palindrome

--- task9/1.java
+++ task9/1.java
@@ -1,32 +1,48 @@
-
-package palindrome;
-import java.util.Scanner;
-public class Palindrome {
+package task9_1;
+import java.util.*;
+public class Task9_1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int temp=n,c=0;
-        int[] count=new int[10];
+        int temp=n,f=0,odd_c=0;
         String str=String.valueOf(n);
+        int[] count=new int[10];
+        
         for(int i=0;i<str.length();i++)
         {
             count[Character.getNumericValue(str.charAt(i))]++;
         }
-        for(int i=0;i<count.length;i++)
-        {
-            if(count[i]==1)
+        
+        int od_ev=(str.length()%2==0)?1:0;
+        if(od_ev==1)
             {
-                c++;
+              for(int i=0;i<count.length;i++)
+                {
+                 if(count[i]%2!=0)
+                 {
+                    f=1;
+                    break;  
+                 }
+                }
+              String x=(f==1)?"NO":"YES";
+              System.out.println(x);
             }
-            System.out.print(count[i]);
+        else
+        {
+            for(int i=0;i<count.length;i++)
+            {
+                if(count[i]%2==1)
+                {
+                    odd_c++;
+                }
+                if(odd_c>1)
+                {
+                    f=2;
+                    break;
+                }
+            }
+              String x=(f!=2)?"YES":"NO";
+              System.out.println(x);
         }
-      if(c>1)
-       {
-           System.out.println("YES");
-       }
-       else
-       {
-           System.out.println("NO");
-       }
     }
 }
